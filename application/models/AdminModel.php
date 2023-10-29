d8be2329e36e44bbba248cd4212c6b5faf6f24ed
Prasanthso <prasanthsubramaniyan945@gmail.com>
2023-10-29 13:27:57 +0530

    adminmodel

--- application/models/AdminModel.php
+++ application/models/AdminModel.php
@@ -35,6 +35,27 @@
             $this->db->insert('admin_login', $insert);
         }
 
+        public function admin()
+        {
+            $admin = "SELECT * FROM `admin_login`";
+            $select = $this->db->query($admin);
+            return array('response' => $select->result_array(), "totalRows" => $select->num_rows());
+        }
+
+        public function employer()
+        {
+            $employer = "SELECT * FROM `provider_registration_form`";
+            $select = $this->db->query($employer);
+            return array('response' => $select->result_array(), "totalRows" => $select->num_rows());
+        }
+
+        public function employee()
+        {
+            $employee = "SELECT * FROM `seeker_profile_form`";
+            $select = $this->db->query($employee);
+            return array('response' => $select->result_array(), "totalRows" => $select->num_rows());
+        }
+
         public function adminUsers()
         {
             $provider = "SELECT * FROM `admin_login`";
@@ -67,8 +88,6 @@
         {
             $postData = $this->input->post(null, true);
             $updateVerificationStatus = array(
-                'userName' => $postData['userName'],
-                'password' => $postData['password'],
                 'verificationStatus' => $postData['verificationStatus'],
                 'verificationRemarks' => $postData['verificationRemarks']
             );
@@ -77,21 +96,35 @@
             $this->db->update('provider_registration_form', $updateVerificationStatus);
         }
 
-        public function unVerifiedCandidates()
+        public function employerApprovelStatus()
         {
-            $unVerifiedCandidates = "SELECT * FROM `seeker_profile_form` WHERE verificationStatus ='0'";
-            $response = $this->db->query($unVerifiedCandidates);
+            $postData = $this->input->post(null, true);
+            $updateVerificationStatus = array(
+                'addNewApprovel' => $postData['addNewApprovel'],
+                'addNewApprovelRemarks' => $postData['addNewApprovelRemarks'],
+                'deleteApprovel' => $postData['deleteApprovel'],
+                'deleteApprovelRemarks' => $postData['deleteApprovelRemarks']
+            );
+
+            $this->db->where('id', $postData['id']);
+            $this->db->update('provider_registration_form', $updateVerificationStatus);
+        }
+
+        public function unVerifiedEmployees()
+        {
+            $unVerifiedEmployees = "SELECT * FROM `seeker_profile_form` WHERE verificationStatus ='0'";
+            $response = $this->db->query($unVerifiedEmployees);
             return $response->result_array();
         }
 
-        public function verifiedCandidates()
+        public function verifiedEmployees()
         {
-            $verifiedCandidates = "SELECT * FROM `seeker_profile_form` WHERE verificationStatus ='1'";
-            $response = $this->db->query($verifiedCandidates);
+            $verifiedEmployees = "SELECT * FROM `seeker_profile_form` WHERE verificationStatus ='1'";
+            $response = $this->db->query($verifiedEmployees);
             return $response->result_array();
         }
 
-        public function verifyCandidateDetails()
+        public function verifyEmployeeDetails()
         {
             $postData = $this->input->post(null, true);
             $updateVerificationStatus = array(
@@ -99,8 +132,36 @@
                 'verificationRemarks' => $postData['verificationRemarks']
             );
 
-            $this->db->where('id', $postData['candidateId']);
+            $this->db->where('id', $postData['EmployeeId']);
             $this->db->update('seeker_profile_form', $updateVerificationStatus);
+        }
+
+        public function addNewAdminApprovel()
+        {
+            $addNewAdminApprovel = "SELECT * FROM `provider_registration_form` WHERE verificationStatus ='1' And addNewRequest ='0'";
+            $response = $this->db->query($addNewAdminApprovel);
+            return array('response' => $response->result_array(), "totalRows" => $response->num_rows());
+        }
+
+        public function deleteAdminApprovel()
+        {
+            $addNewAdminApprovel = "SELECT * FROM `provider_registration_form` WHERE verificationStatus ='1' And addNewRequest ='0'";
+            $response = $this->db->query($addNewAdminApprovel);
+            return array('response' => $response->result_array(), "totalRows" => $response->num_rows());
+        }
+
+        public function addNewEmployerApprovel()
+        {
+            $addNewAdminApprovel = "SELECT * FROM `provider_registration_form` WHERE verificationStatus ='1' And addNewRequest ='1' And addNewApprovel ='0'";
+            $response = $this->db->query($addNewAdminApprovel);
+            return array('response' => $response->result_array(), "totalRows" => $response->num_rows());
+        }
+
+        public function deleteEmployerApprovel()
+        {
+            $addNewAdminApprovel = "SELECT * FROM `provider_registration_form` WHERE verificationStatus ='1' And addNewApprovel ='1' And deleteRequest ='1' And deleteApprovel ='0'";
+            $response = $this->db->query($addNewAdminApprovel);
+            return array('response' => $response->result_array(), "totalRows" => $response->num_rows());
         }
     }
 
